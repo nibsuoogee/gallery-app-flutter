7d8f38a9837c58a7e7c5d1014453969a66f38961
nibsuoogee <elias.syyrila@gmail.com>
2023-02-07 19:02:14 +0200

    Infinite pages added, lazy style loading, back to top button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -33,13 +35,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   List<String> _searchArray = ['nature', 'beautiful', 'flowers', 'landscape', 'abstract', 'fire', 'dark', 'love', 'winter'];
   String selectedWord = 'None';
   List<String> _images = [];
+  final ScrollController _scrollController = ScrollController();
+
+  void scrollToTop() {
+    _scrollController.jumpTo(0.0);
+  }
+
+  int calculatePageNumber(int imageNumber) {
+    int nextPage = (imageNumber/15).floor() + 1;
+    return ((nextPage >= 1) ? nextPage : 1);
+  }
 
   void makeApiRequest(String searchWord) async {
-    Uri myuri = Uri.parse('https://api.pexels.com/v1/search?query=$searchWord');
+    if (searchWord.isEmpty) {return;}
+
+    int page = calculatePageNumber(_images.length);
+    Uri myuri = Uri.parse('https://api.pexels.com/v1/search?query=$searchWord&page=$page');
     final response = await http.get(
       myuri,
       headers: {
@@ -48,7 +62,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body) as Map;
-      final imageUrl = 'what';
 
       List<String> responseImages = [];
 
@@ -57,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
       }
 
       setState(() {
-        _images = responseImages;
+        _images = _images + responseImages;
       });
       return;
 
@@ -66,12 +79,18 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  void addCollie() {
+    setState(() {
+      _images.add("https://www.vastavalo.net/albums/userpics/13414/normal_4564_collie.jpg");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Image.asset('graphics/gg_logo.png', fit: BoxFit.contain, height: 100),
       ),
 
       body: Container(
@@ -80,8 +99,9 @@ class _MyHomePageState extends State<MyHomePage> {
         direction: Axis.vertical,
         children: [
          Expanded(
-          child: SizedBox(           
+          child: SizedBox(
             child: ListView.builder(
+            controller: _scrollController,
             itemCount: _images.length + 1,
             itemBuilder: (context, int index){
             if (index == 0) {
@@ -92,24 +112,76 @@ class _MyHomePageState extends State<MyHomePage> {
                     onWordSelected: (word) {
                       makeApiRequest(word);
                       setState(() {
+                        _images = [];
                         selectedWord = word;
                       });
                     },
                   ),
                 );
+            } else if (index == (_images.length)) {
+              return Column(children: [
+                Text(
+                      textAlign: TextAlign.center,
+                      '${_images.length} loaded (page ${calculatePageNumber(_images.length)})', style: TextStyle(color: Color.fromARGB(255, 99, 99, 99)),
+                    ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                  onTap: () {
+                    makeApiRequest(selectedWord);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color.fromARGB(255, 224, 224, 224),
+                      width: 2,
+                    ),
+                    ),
+                    child: const Text(
+                      textAlign: TextAlign.center,
+                      'Load more images', style: TextStyle(color: Color.fromARGB(255, 197, 197, 197)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                  onTap: () {
+                    scrollToTop();
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color.fromARGB(255, 130, 130, 130),
+                      width: 2,
+                    ),
+                    ),
+                    child: const Text(
+                      textAlign: TextAlign.center,
+                      'Back to top', style: TextStyle(color: Color.fromARGB(255, 99, 99, 99)),
+                    ),
+                  ),
+                ),
+              )
+              ]);
+              
             } else {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10.0),
-                  child: Image.network(
-                    _images[index-1],
-                  ),
+                  child: FadeInImage.assetNetwork(
+                      placeholder: 'graphics/dark_placeholder_image.jpg',
+                      image: _images[index-1],
+                    ),
                 ),
               );
             }
           },)
-          ))
+          )),
         ]
       ),
       )
@@ -148,7 +220,7 @@ class _SearchSelectMatrix extends State<SearchSelectMatrix> {
                 padding: EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   border: Border.all(
-                    color: word == selectedWord ? Colors.blue : Colors.grey,
+                    color: word == selectedWord ? Color.fromARGB(255, 231, 20, 20) : Color.fromARGB(255, 189, 189, 189),
                     width: 2,
                   ),
                 ),
